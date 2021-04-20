1df7076089cc30c4643f9c2e02f586a7b176ef10
CAEB <CAEB@users.noreply.github.com>
2021-04-20 09:33:55 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 13ff0e3caf3dd71e51b4bca5e6d49e9f460ea4d4

---
+++ assets/axios.38264479.js
@@ -0,0 +1,1 @@
+import{d as e,o as r,c as o}from"./vendor.ef0632be.js";var a=e({setup(){}});a.render=function(e,a,n,s,t,u){return r(),o("h1",null,"axios")};export default a;
---
+++ assets/home.ba9dc0a3.js
@@ -0,0 +1,1 @@
+import{d as e,o as r,c as o}from"./vendor.ef0632be.js";var n=e({setup(){}});n.render=function(e,n,t,a,s,u){return r(),o("h1",null,"home")};export default n;
